bbe2faed77ca7b42118dc39753fafb26b2949263
Noah Hummel <n.p.hummel@gmail.com>
2019-03-02 19:18:18 +0100

    [FEATURE] ADD client PublicV1alpha/BlogEntry

---
+++ client/PublicV1alpha/BlogEntry.js
@@ -0,0 +1,24 @@
+import callApi from "../common";
+import BlogEntry from "../../tiny-blog-model/v1alpha/BlogEntry";
+
+const baseUrl = "localhost:8082";
+
+function getBlogEntriesByTag(tags, offset, limit) {
+    return callApi(baseUrl, "/entries/byTag", "GET", {
+        query: {offset, limit},
+        body: tags
+    })
+        .map(BlogEntry.unMarshal);
+}
+
+function getBlogEntriesByNewest(offset, limit) {
+    return callApi(baseUrl, "/entries/newest", "GET", {
+        query: {offset, limit}
+    })
+        .map(BlogEntry.unMarshal);
+}
+
+export {
+    getBlogEntriesByNewest,
+    getBlogEntriesByTag
+};
